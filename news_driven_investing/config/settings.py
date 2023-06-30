54250829009d6d667d477e70ae4205a26c367628
lerummi <martin.krause.85@gmx.de>
2023-06-30 09:52:19 +0200

    Updated start date and channels to completeness

--- news_driven_investing/config/settings.py
+++ news_driven_investing/config/settings.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 
 class Settings(BaseSettings):
 
-    START_DATE = "2023-01-01"
+    START_DATE = "2022-06-01"
 
     YOUTUBE_CHANNELS: Dict[str, str] = {
         "CNN": "UCupvZG-5ko_eiXAupbDfxWw",
@@ -13,18 +13,18 @@ class Settings(BaseSettings):
         "Bloomberg Originals": "UCUMZ7gohGI9HcU9VNsr2FJQ",
         "NBC News": "UCeY0bbntWzzVIaj2z3QigXg",
         "NBC": "UCccjdJEay2hpb5scz61zY6Q",
-        #"CNBC Television": "UCrp_UI8XtuYfpiqluWLD7Lw",
-        #"CNBC": "UCvJJ_dzjViJCoLf5uKUTwoA",
-        #"CNBC International TV": "UCF8HUTbUwPKh2Q-KpGOCVGw",
-        #"Fox News": "UCXIJgqnII2ZOINSWNOGFThA",
-        #"ABC News": "UCBi2mrWuNuyYy4gbM6fU18Q",
-        #"Yahoo Finance 1": "UCEAZeUIeJs0IjQiqTCdVSIg",
-        #"Yahoo Finance 2": "UC0kuWfYTcAgKW2p36W7bpnw",
-        #"CNBC Make It": "UCH5_L3ytGbBziX0CLuYdQ1Q",
-        #"Bloomberg Technology": "UCrM7B7SL_g1edFOnmj-SDKg",
-        #"NBC Digital News": "UCvQbrQHNdcofvya0aG7jEjA",
-        #"ABC News In-depth": "UCxcrzzhQDj5zKJbXfIscCtg",
-        #"Cheddar": "UC04KsGq3npibMCE9Td3mVDg"
+        "CNBC Television": "UCrp_UI8XtuYfpiqluWLD7Lw",
+        "CNBC": "UCvJJ_dzjViJCoLf5uKUTwoA",
+        "CNBC International TV": "UCF8HUTbUwPKh2Q-KpGOCVGw",
+        "Fox News": "UCXIJgqnII2ZOINSWNOGFThA",
+        "ABC News": "UCBi2mrWuNuyYy4gbM6fU18Q",
+        "Yahoo Finance 1": "UCEAZeUIeJs0IjQiqTCdVSIg",
+        "Yahoo Finance 2": "UC0kuWfYTcAgKW2p36W7bpnw",
+        "CNBC Make It": "UCH5_L3ytGbBziX0CLuYdQ1Q",
+        "Bloomberg Technology": "UCrM7B7SL_g1edFOnmj-SDKg",
+        "NBC Digital News": "UCvQbrQHNdcofvya0aG7jEjA",
+        "ABC News In-depth": "UCxcrzzhQDj5zKJbXfIscCtg",
+        "Cheddar": "UC04KsGq3npibMCE9Td3mVDg"
     }
 
     YOUTUBE_API_KEY: str = Field("", env="YOUTUBE_API_KEY")
@@ -43,7 +43,10 @@ class Settings(BaseSettings):
     ALPHA_VANTAGE_SYMBOLS: Dict[str, str] = {
         "Google": "GOOGL",
         "Microsoft": "MSFT",
-        "Apple": "AAPL"
+        "Apple": "AAPL",
+        "Meta": "FB2A",
+        "Amazon": "AMZ",
+        "Netflix": "NFC"
     }
 
 settings = Settings()
